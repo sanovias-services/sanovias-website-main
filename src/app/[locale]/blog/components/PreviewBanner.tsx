0011b39660fe5bfaab43b41cd36c495ac5047890
aselmi <selmi.aladeen@gmail.com>
2025-10-02 13:55:35 +0200

    Rename of preview-utils to preview-client

--- src/app/[locale]/blog/components/PreviewBanner.tsx
+++ src/app/[locale]/blog/components/PreviewBanner.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { getPreviewModeFromCookies, PreviewInfo } from '@/lib/contentful/preview-utils';
+import { getPreviewModeFromCookies, PreviewInfo } from '@/lib/contentful/preview-client';
 
 /**
  * Preview Mode Banner
